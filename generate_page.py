e168a35b0bb6f17766bfcd60ea264035b4c23906
Vaclav Petras <wenzeslaus@gmail.com>
2022-03-08 10:06:49 -0500

    Format code with black (#5)
    * Add black
    * Update generate_page.py

--- generate_page.py
+++ generate_page.py
@@ -3,19 +3,19 @@
 import dominate
 from dominate import tags
 
-doc = dominate.document(title='Dominate your HTML')
+doc = dominate.document(title="Dominate your HTML")
 
 with doc.head:
-    tags.link(rel='stylesheet', href='style.css')
-    tags.script(type='text/javascript', src='script.js')
+    tags.link(rel="stylesheet", href="style.css")
+    tags.script(type="text/javascript", src="script.js")
 
 with doc:
-    with tags.div(id='header').add(tags.ol()):
-        for i in ['home', 'about', 'contact']:
-            tags.li(tags.a(i.title(), href=f'/{i}.html'))
+    with tags.div(id="header").add(tags.ol()):
+        for i in ["home", "about", "contact"]:
+            tags.li(tags.a(i.title(), href=f"/{i}.html"))
 
     with tags.div():
-        tags.attr(cls='body')
-        tags.p('Lorem ipsum..')
+        tags.attr(cls="body")
+        tags.p("Lorem ipsum..")
 
 print(doc)
